a083d227eebb143caff85b615814cd044a4a7633
Sampo Lamminsalo <sampo.lamminsalo@protonmail.com>
2020-02-01 18:24:05 +0200

    Ladder movement

--- Ballast.gd
+++ Ballast.gd
@@ -4,9 +4,19 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+signal balance_change
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func get_balance():
+	if $LeftBallast/LeftBalance.current_level == $RightBallast/RightBalance.current_level:
+		return 0
+	if $LeftBallast/LeftBalance.current_level > $RightBallast/RightBalance.current_level:
+		return -1
+	else:
+		return 1
 
 func action(side : Sprite):
 	if(side.is_broken()):
@@ -17,4 +27,4 @@ func action(side : Sprite):
 	else:
 		$LeftBallast/LeftBalance.lower()
 		$RightBallast/RightBalance.higher()
-			
+	emit_signal("balance_change")
