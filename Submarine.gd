42f08e097cf620458eaf3a970d65ea6c61eab5ad
Sampo Lamminsalo <sampo.lamminsalo@protonmail.com>
2020-02-01 23:48:52 +0200

    fire backend

--- Submarine.gd
+++ Submarine.gd
@@ -3,13 +3,27 @@ extends Node2D
 var damage = 0
 export var max_fire_chance = 0.5
 export var min_fire_chance = 0.95
-
+var fires_top = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var fires_bot = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var fires = [fires_top, fires_bot];
 
 func _ready():
 	randomize()
 
 func spawn_fire():
-	print("fire!!")
+	var free_spots = get_free_fire()
+	var spot = free_spots[randi() % free_spots.size()];
+	fires[spot.y][spot.x] = 1
+
+func get_free_fire():
+	var free = Array()
+	for x in range(fires_top.size()):
+		if fires_top[x] == 0:
+			free.append(Vector2(x, 0))
+	for x in range(fires_bot.size()):
+		if fires_bot[x] == 0:
+			free.append(Vector2(x, 1))
+	return free
 
 func _on_Ballasts_balance_change():
 	var balance = $Ballasts.get_balance()
